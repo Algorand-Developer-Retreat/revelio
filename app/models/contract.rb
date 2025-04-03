f9bf40913347a5c5b18311cf8a8579ff34bc40a1
loedn <loedn@pm.me>
2025-04-03 10:56:43 +0200

    stuff

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -417,7 +417,7 @@ class Contract < ApplicationRecord
     
     # Basic types
     basic_types = [
-      'uint8', 'uint16', 'uint32', 'uint64', 'uint128', 'uint256',
+      'uint8', 'uint16', 'appl', 'uint32', 'uint64', 'uint128', 'uint256',
       'byte', 'bytes', 'string', 'address', 'bool', 'void', 'AVMString',
       'pay', 'account', 'application', 'asset', 'AVMBytes', 'AVMUint64'
     ]
